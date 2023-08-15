7ad42b396e559e203234a282be486645465af2a4
LeviTC <erletaco@outlook.com>
2023-08-14 23:41:52 -0600

    Levantamiento de servidor y conección a DB

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const indexRouter = require("./routes/indexRouter");
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const dataBase = require("./dataBase");
 
 require("dotenv").config();
 const { PORT } = process.env;
-// console.log(dataBase);
+
 app.listen(PORT, () => {
     dataBase();
     console.log(`Server on port: http://localhost:${PORT}`);
