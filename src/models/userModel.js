66cafec06a79e577df230dedf1c933b272bedb77
LeviTC <123846277+LeviTC@users.noreply.github.com>
2023-08-15 15:00:53 -0600

    Main l (#6)
    * Permisos
    
    * prueba2
    
    * sad
    
    * ssassa
    
    * Creación de Archivos
    
    * Levantamiento de servidor y conección a DB
    
    * Creacion de UserModel
    
    * Feat (back/User Model) Model added

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
 
 
 const userSchema = new Schema({
@@ -14,9 +15,52 @@ const userSchema = new Schema({
             required: true
         },
     },
-}
-);
+    age: {
+        type: Number,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    hasHotels: false
 
-const User = mongoose.model("User", userSchema);
-// console.log(User);
+}, { timestamps: true });
+
+
+//              Metodos
+
+//Encriptar Password
+userSchema.methods.encryptPassword = async (password) => {
+    let salt = await bcrypt.genSalt(10);
+    let ecnrypted = await bcrypt.hash(password, salt);
+    return ecnrypted;
+};
+
+//Matchear password para login
+userSchema.methods.matchPassword = async function (password) {
+    const matched = await bcrypt.compare(password, this.password);
+    return matched;
+};
+
+//Creación de modelo
+const User = model("User", userSchema);
 module.exports = User;
