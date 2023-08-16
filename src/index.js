d0d0ae9cc857157c6420b0a78abb153e48e84d50
LeviTC <erletaco@outlook.com>
2023-08-16 10:46:49 -0600

    Feat (Back/hotelPostRoute) added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const dataBase = require("./dataBase");
 
 require("dotenv").config();
 const { PORT } = process.env;
-// console.log(dataBase);
 app.listen(PORT, () => {
     dataBase();
     console.log(`Server on port: http://localhost:${PORT}`);
