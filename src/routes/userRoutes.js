57e057e9f625cf53b84a452be26a438875c80cab
Ana María Orozco <103206919+AnaO-97@users.noreply.github.com>
2023-08-18 00:57:24 -0500

    Ruta user get (#20)
    * Ruta Put completa y funcional
    
    * Ruta Get/:id completa y funcional
    
    ---------
    
    Co-authored-by: ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,15 @@
 const { Router } = require('express');
-const userPost = require("../handlers/userPost.js")
-const userGet = require("../handlers/userGet.js")
-const userDelete = require("../handlers/userDelete.js")
-const userPut = require("../handlers/userPut.js")
+const userPost = require("../handlers/userPost.js");
+const userGet = require("../handlers/userGet.js");
+const userGetID = require("../handlers/userGetID.js");
+const userDelete = require("../handlers/userDelete.js");
+const userPut = require("../handlers/userPut.js");
 
 const userRoutes = Router();
 
 userRoutes.post("/", userPost);
 // userRoutes.get("/", userGet);
+userRoutes.get("/:id", userGetID);
 // userRoutes.delete("/", userDelete);
 userRoutes.put("/", userPut);
 
