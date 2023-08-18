24e757d8ba1c607a13fc82fa087dd257d9f3b3bc
LeviTC <123846277+LeviTC@users.noreply.github.com>
2023-08-18 00:01:45 -0600

    Feta (back/HotelRoutes) byName, byCuntry byCity (#21)
    Cree las rutas pra obtener Hoteles

---
+++ src/controllers/getHotelByName.js
@@ -0,0 +1,9 @@
+const Hotel = require("../models/hotelModel");
+
+const getHotelByName = async (name) => {
+    const hotelFound = await Hotel.find({ name: name });
+    if (hotelFound.length) return hotelFound;
+    throw new Error("Hotel not found.");
+};
+
+module.exports = getHotelByName;
