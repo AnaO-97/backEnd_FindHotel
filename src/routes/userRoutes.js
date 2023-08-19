ac59dc926524f377d2180f67967e104195ef5b47
Ana María Orozco <103206919+AnaO-97@users.noreply.github.com>
2023-08-18 19:59:55 -0500

    Validacion en ruta PostUser (#23)
    Co-authored-by: ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
+const validationUserPost = require("../middlewares/validationUserPost")
 const userPost = require("../handlers/userPost.js");
-const userGet = require("../handlers/userGet.js");
+// const userGet = require("../handlers/userGet.js");
 const userGetID = require("../handlers/userGetID.js");
-const userDelete = require("../handlers/userDelete.js");
+// const userDelete = require("../handlers/userDelete.js");
 const userPut = require("../handlers/userPut.js");
 
 const userRoutes = Router();
 
-userRoutes.post("/", userPost);
+userRoutes.post("/", validationUserPost, userPost);
 // userRoutes.get("/", userGet);
 userRoutes.get("/:id", userGetID);
 // userRoutes.delete("/", userDelete);
