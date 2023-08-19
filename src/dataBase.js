1fad48425b217ffda8ab6534dfe068e51e2dad3b
LuisFalconS <125808185+Miguel7F@users.noreply.github.com>
2023-08-19 00:05:35 -0500

    fit (back/Database-hotelRoutes) Se confirman los cambios a realizar

--- src/dataBase.js
+++ src/dataBase.js
@@ -6,11 +6,7 @@ const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 const dataBase = () => {
 
 
-<<<<<<< HEAD
-    mongoose.connect(`mongodb+srv://${DB_NAME}:${DB_PASSWORD}@${DB_USER}.lmykssy.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {
-=======
     mongoose.connect(`mongodb+srv://${DB_NAME}:${DB_PASSWORD}@${DB_USER}.lmykssy.mongodb.net?retryWrites=true&w=majority`, {
->>>>>>> 739b31c69421773d55e77ad8cd3d41ddf6ded370
 
         useNewUrlParser: true,
         useUnifiedTopology: true,
