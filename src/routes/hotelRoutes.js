fd2a7cb7d70de893a3379b6b9a786c3f175cec7d
LuisFalconS <125808185+Miguel7F@users.noreply.github.com>
2023-08-19 13:53:32 -0500

    Luis branch (#27)
    * Fit(back/trending routes) add validationTrendingStatesPost
    
    * fit (back/Database-hotelRoutes) Se confirman los cambios a realizar

--- src/routes/hotelRoutes.js
+++ src/routes/hotelRoutes.js
@@ -2,14 +2,10 @@ const { Router } = require('express');
 const hotelPost = require("../handlers/hotelPost.js")
 const hotelGet = require("../handlers/hotelGet.js")
 const hotelDelete = require("../handlers/hotelDelete.js")
-<<<<<<< HEAD
 const hotelPut = require("../handlers/hotelPut.js");
 const hotelsByCountryGet = require('../handlers/hotelsByCountryGet.js');
 const hotelsByStateGet = require('../handlers/hotelsByStateGet.js');
 const hotelByNameGet = require('../handlers/hotelByNameGet.js');
-=======
-const hotelPut = require("../handlers/hotelPut.js")
->>>>>>> 739b31c69421773d55e77ad8cd3d41ddf6ded370
 
 const hotelRoutes = Router();
 
