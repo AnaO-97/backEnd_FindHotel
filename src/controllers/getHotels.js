51a581c80446bda66904f237426606d90cc02943
LuisFalconS <125808185+Miguel7F@users.noreply.github.com>
2023-08-21 16:01:27 -0500

    Luis branch (#33)
    * Merge branch 'luisBranch' of https://github.com/AnaO-97/backEnd_FindHotel into luisBranch
    
    * Fit(back/treding States routes) remove validation
    
    * Feat(back/model Countries)Add model countries
    
    * Feat (back/hotels route) add get function

---
+++ src/controllers/getHotels.js
@@ -0,0 +1,25 @@
+const Hotel = require("../models/hotelModel");
+
+const getHotelList = async (key, name) => {
+    return await Hotel.find({ [key]: { $regex: name, $options: "i" } });
+};
+
+const getHotels = (name) => {
+    try {
+        return Promise.all([
+            getHotelList("country", name),
+            getHotelList("state", name),
+            getHotelList("name", name)
+        ])
+            .then((values) => {
+                return values
+            })
+            .catch((error) => {
+                throw Error(error)
+            })
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+module.exports = getHotels;
