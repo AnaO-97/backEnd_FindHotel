1411e2f376e31c198c02ecbb4ba399d22c435016
LuisFalconS <125808185+Miguel7F@users.noreply.github.com>
2023-08-21 15:59:08 -0500

    Feat (back/hotels route) add get function

--- src/handlers/hotelGet.js
+++ src/handlers/hotelGet.js
@@ -1,12 +1,12 @@
-const hotelCreate = require("../controllers/hotelCreate");
+const getHotels = require("../controllers/getHotels.js");
 
-const hotelGet = async (req, res) => {
+const  hotelGet= async (req, res) => {
     try {
-        const attributes = req.body;
-        const newHotel = await hotelCreate(attributes);
-        res.status(200).json(newHotel);
+        const {name} = req.query;
+        const hotelsFound = await getHotels(name);
+        res.status(200).json(hotelsFound);
     } catch (error) {
-        res.status(404).json({ message: error });
+        res.status(404).json({ error: error.message });
     };
 };
 
