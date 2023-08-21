990bea837512ee9b6ff01b77bd240d0363a4027e
LuisFalconS <125808185+Miguel7F@users.noreply.github.com>
2023-08-21 00:15:56 -0500

    Luis branch (#31)
    * Merge branch 'luisBranch' of https://github.com/AnaO-97/backEnd_FindHotel into luisBranch
    
    * Fit(back/treding States routes) remove validation

--- src/handlers/trendingStatesPost.js
+++ src/handlers/trendingStatesPost.js
@@ -2,7 +2,7 @@ const postTrendingStates = require("../controllers/postTrendingStates.js")
 
 const trendingStatesPost=async(req, res)=>{
     try{
-        const {attributes} = req.body;
+        const attributes = req.body;
         const trendingStates=await postTrendingStates(attributes)
         res.status(200).json(trendingStates);
     }catch(err){
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,9 +3,9 @@ const { validationResult } = require('express-validator');
 
 const validationUserPost = require("../middlewares/validationUserPost")
 const userPost = require("../handlers/userPost.js");
-// const userGet = require("../handlers/userGet.js");
+const userGet = require("../handlers/userGet.js");
 const userGetID = require("../handlers/userGetID.js");
-// const userDelete = require("../handlers/userDelete.js");
+const userDelete = require("../handlers/userDelete.js");
 const userPut = require("../handlers/userPut.js");
 
 const userRoutes = Router();
