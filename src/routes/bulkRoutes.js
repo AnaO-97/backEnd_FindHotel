42ea3b32d5b1e553705c91c597222bb1f427eea3
LeviTC <erletaco@outlook.com>
2023-08-21 23:10:47 -0600

    Feat (back/HotelDetail) Hotel Detail Route

--- src/routes/bulkRoutes.js
+++ src/routes/bulkRoutes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
-const bulkHandler = require("../handlers/bulkHandler.js")
+const bulkHandler = require("../handlers/bulkHandler.js");
 const bulkRoutes = Router();
 
-bulkRoutes.post("/", bulkHandler)
+bulkRoutes.post("/", bulkHandler);
 
 module.exports = bulkRoutes;
