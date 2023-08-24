ef431952bb6ba8ae5f7fd5bccc7e65cac1feb38a
Karol Castilla Cruz <karolcastilla@hotmail.com>
2023-08-23 22:52:38 -0500

    Comment dotenv config

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const app = require("./src/app");
 const dataBase = require("./src/dataBase");
 const config = require("./src/config/config");
 
-app.listen(config.PORT, () => {
+const { PORT } = process.env;
+app.listen(PORT, () => {
     dataBase();
     console.log(`Server on port: http://localhost:${ config.PORT  }`);
 });
