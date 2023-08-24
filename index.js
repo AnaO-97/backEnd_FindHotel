f766769661367dfc1175e9f620dc672f7a99face
Karol Castilla Cruz <karolcastilla@hotmail.com>
2023-08-23 23:01:32 -0500

    port config

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const app = require("./src/app");
 const dataBase = require("./src/dataBase");
 const config = require("./src/config/config");
 
-const { PORT } = process.env;
-app.listen(PORT, () => {
+app.listen(config.PORT, () => {
     dataBase();
     console.log(`Server on port: http://localhost:${ config.PORT  }`);
 });
