783218b669441258b57dacc6ec095ea7a75bd314
LuisFalconS <125808185+Miguel7F@users.noreply.github.com>
2023-08-23 21:18:54 -0500

    Luis branch (#39)
    * Merge branch 'luisBranch' of https://github.com/AnaO-97/backEnd_FindHotel into luisBranch
    
    * Fit(back/treding States routes) remove validation
    
    * Feat(back/model Countries)Add model countries
    
    * Feat (back/hotels route) add get function
    
    * Feat (back/hotels route) add get function
    
    * feat(back/put hotel route) add put hotel route
    
    * feat(back/mercado pago)Intregración con mercado pago

---
+++ src/handlers/createOrderPost.js
@@ -0,0 +1,15 @@
+const createOrder = require('../controllers/createOrder.js')
+
+const createOrderPost = async (req, res) => {
+    try {
+        const attributes = req.body;
+        const response = await createOrder(attributes)
+        console.log(response)
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+};
+
+module.exports = createOrderPost;
+
