873d1135e81057aa693ca09679b8602de4578d4c
LeviTC <erletaco@outlook.com>
2023-08-24 23:59:20 -0600

    Feat (back/postHotelRoom) Add route

---
+++ src/controllers/hotelRoom/getHoteRoomByUserId.js
@@ -0,0 +1,14 @@
+const HotelRoom = require("../../models/hotelRoomModel");
+
+
+const getHotelRoomByUserId = async (req, res) => {
+    try {
+        const { User_id, Hotel_id, roomType_id } = req.body;
+
+    } catch (error) {
+
+    }
+};
+
+
+module.exports = getHotelRoomByUserId;
