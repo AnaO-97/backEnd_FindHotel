bfa9671c339e39acb67960e966f252d00b1e0909
LuisFalconS <125808185+Miguel7F@users.noreply.github.com>
2023-08-31 05:27:10 -0500

    Luis branch (#63)
    * Feat (back MP Route) update Deal model, config recibeWebHook, update create order route
    
    * feat(back / model States) add states models and fix countries model. update Countries json

--- src/controllers/postBulk.js
+++ src/controllers/postBulk.js
@@ -8,3 +8,64 @@ const postCountries = async (attributes) => {
 };
 
 module.exports = postCountries;
+
+
+/*------------------------------------------------------------------------------------------------------------*/
+
+// //const Countries = require("../models/countries.js");
+// const Hotel = require("../models/hotelModel.js");
+// const hotels = require('../../hotels.json')
+// const { v4: uuidv4 } = require('uuid');
+
+// const postCountries = async (attributes) => {
+//     console.log(attributes);
+//     console.log(attributes.id);
+//     // const countriesBulk = new Countries(attributes);
+//     // return await countriesBulk.create();
+//     // const hotel = await Hotel.findByIdAndUpdate(attributes.id);
+
+//     hotels.forEach(hotel => {
+//         const hotelNew = {
+//             ...hotel,
+//             image: [{
+//                 id: uuidv4(),
+//                 src: hotel.image,
+//                 typeImage: "image/jpeg",
+//                 size: 128419
+//             }]
+//         }
+//         const hotelsBulk = new Hotel(hotelNew);
+//         hotelsBulk.save();
+//     });
+//     return 'ok'
+    
+    
+// };
+/*------------------------------------------------------------------------------------------------------------*/
+
+// module.exports = postCountries;
+
+// const axios = require("axios");
+// const States = require("../models/states.js");
+// const Countries = require("../models/countries.js");
+
+// const postCountries = async (attributes) => {
+//     const name = attributes.country_name
+
+//     const { data } = await axios({
+//         method: 'get',
+//         url: `https://www.universal-tutorial.com/api/states/${name}`,
+//         headers: {
+//             Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJmaW5kaG90ZWxmaEBnbWFpbC5jb20iLCJhcGlfdG9rZW4iOiI1eUV3eXRHYWtKd0ZGQWRlenM2UHN4UjNwTG12Q3dlbi1xSnBhSG51cVdZT00tcEZBR1F5QWtkWEFNWXQxWXFJbHZFIn0sImV4cCI6MTY5MzUxMzg0MX0.MCjzdlXN4fP8TKcl5crpP3ImDdue2yJLuJpcDaSCLXk',
+//             Accept: 'application/json'
+//         }
+//     })
+
+//     const { _id } = await Countries.findOne({ country_name: name });
+//     const devolver = new States({ Countries_id: _id, states: data })
+//     return await devolver.save();
+//     // return devolver
+
+// };
+
+// module.exports = postCountries;
