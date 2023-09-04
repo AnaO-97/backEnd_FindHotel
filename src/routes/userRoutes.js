8c1039b5e50ea0c7c40bae04de037e936f4b0379
daochoam <daochoam@unal.edu.co>
2023-09-04 18:54:27 -0500

    Configuration Sessions

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const { validationResult } = require('express-validator');
-
 const validationUserPost = require("../middlewares/validationUserPost")
 const userPost = require("../handlers/user/userPost.js");
 const userGet = require("../handlers/user/userGet.js");
