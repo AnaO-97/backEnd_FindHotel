3b7dd64a7b6d3f986933f8e687ab80beeef19405
Daniel F. Ochoa M <81942715+daochoam@users.noreply.github.com>
2023-09-06 02:31:30 -0500

    Delete Specific CORS

--- src/app.js
+++ src/app.js
@@ -12,26 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.all('*', function (request, response, next) {
-    var whitelist = request.headers.origin;
-    response.header('Access-Control-Allow-Origin', whitelist)
-    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,HEAD');
-    response.header('Access-Control-Allow-Headers', " authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    response.header("Access-Control-Allow-Credentials", "true");
-    next()
-})
-
-app.use(cors({
-    origin: function (origin, callback) {
-        console.log(origin)
-        if (!origin) return callback(null, true)
-
-        if (config.DOMAINS.indexOf(origin) === -1) {
-            return callback('error de cors', false)
-        }
-        return callback(null, true)
-    }
-}))
+app.use(cors())
 
 app.use(cookieParser());
 // Config la session
